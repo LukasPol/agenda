4a07717823931b9c09c6024c8ac1afb323e2a0ec
Lukas Pol <lukaspaespol@gmail.com>
2019-07-16 15:10:06 -0300

    Filtro para datas maiores q a atual

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 
@@ -22,3 +23,9 @@ class Evento(models.Model):
 
     def get_data_evento_input(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
--- app/views.py
+++ app/views.py
@@ -3,8 +3,69 @@ from app.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime,timedelta
 
 # Create your views here.
+@login_required(login_url='/login/')
+def listaEvento(request):
+    usuario = request.user
+    eventos = Evento.objects.filter(usuario=usuario).order_by('data_evento')
+    # eventos = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos' :  eventos}
+    return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    idEvento = request.GET.get('id')
+    dados = {}
+    if idEvento:
+        dados['evento'] = Evento.objects.get(id=idEvento)
+    return render(request, 'evento.html', dados)
+
+def verificaData(data_evento):
+    dataAtual = datetime.now()
+    data_evento = data_evento.replace('T', ' ')
+    data_evento_time = datetime.strptime(data_evento, '%Y-%m-%d %H:%M')
+    print(data_evento)
+    print(dataAtual)
+    if data_evento_time>dataAtual: d = True
+    else: d = False
+    return d
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        idEvento = request.POST.get('id')
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data_evento = request.POST.get('data_evento')
+        usuario = request.user
+        d = verificaData(data_evento)
+        if d == False and not idEvento:
+            messages.error(request, "Insira uma data futura")
+            return redirect('/agenda/evento/')
+        elif d == False and idEvento:
+            messages.error(request, "Insira uma data futura")
+        elif idEvento and d == True:
+            evento = Evento.objects.get(id=idEvento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.descricao = descricao
+                evento.data_evento = data_evento
+                evento.save()
+                messages.success(request, 'Alteração com sucesso')
+        elif not idEvento and d == True:
+            Evento.objects.create(titulo=titulo, descricao=descricao, data_evento=data_evento, usuario=usuario)
+            messages.success(request, 'Criado com sucesso')
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id):
+    usuario = request.user
+    evento = Evento.objects.get(id=id)
+    if evento.usuario == usuario:
+        evento.delete()
+    return redirect('/')
 
 def login_user(request):
     return render(request, 'login.html')
@@ -22,48 +83,4 @@ def submit_login(request):
 
 def logout_user(request):
     logout(request)
-    return redirect('/')
-
-@login_required(login_url='/login/')
-def listaEvento(request):
-    usuario = request.user
-    eventos = Evento.objects.filter(usuario=usuario)
-    dados = {'eventos' :  eventos}
-    return render(request, 'agenda.html', dados)
-
-@login_required(login_url='/login/')
-def evento(request):
-    idEvento = request.GET.get('id')
-    dados = {}
-    if idEvento:
-        dados['evento'] = Evento.objects.get(id=idEvento)
-    return render(request, 'evento.html', dados)
-
-@login_required(login_url='/login/')
-def submit_evento(request):
-    if request.POST:
-        idEvento = request.POST.get('id')
-        titulo = request.POST.get('titulo')
-        descricao = request.POST.get('descricao')
-        data_evento = request.POST.get('data_evento')
-        usuario = request.user
-        if idEvento:
-            evento = Evento.objects.get(id=idEvento)
-            if evento.usuario == usuario:
-                evento.titulo = titulo
-                evento.descricao = descricao
-                evento.data_evento = data_evento
-                evento.save()
-            # Evento.objects.filter(id=idEvento).update(titulo=titulo, descricao=descricao, data_evento=data_evento) 
-        else:
-            Evento.objects.create(titulo=titulo, descricao=descricao, data_evento=data_evento, usuario=usuario)
-
-    return redirect('/')
-
-@login_required(login_url='/login/')
-def delete_evento(request, id):
-    usuario = request.user
-    evento = Evento.objects.get(id=id)
-    if evento.usuario == usuario:
-        evento.delete()
     return redirect('/')
